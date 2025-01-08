bcfbc8dfd6ea82aebfafe2cd925188b41ac8f10d
MaxAgui <manuelmaximo19@gmail.com>
2025-01-07 19:44:58 -0500

    chore(init): inicializar proyecto con start spring

---
+++ src/main/java/com/example/tipo_cambio/PruebaTipoCambioApplication.java
@@ -0,0 +1,13 @@
+package com.example.tipo_cambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTipoCambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTipoCambioApplication.class, args);
+	}
+
+}
